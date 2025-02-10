0320e2ce5928269d8e514c185e609c5c905b22aa
JSLee <josuk0212@gmail.com>
2025-02-10 11:47:49 +0900

    feat: 초기페이지-파일 업로드 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import FileUpload from "./FileUpload";
+
 function App() {
   return (
     <>
-      <h1>Init</h1>
+      <h1 className="flex justify-center text-5xl mt-16">Welcome H P</h1>
+      <div className="flex justify-center text-2xl mt-6">Help Presentation</div>
+      <FileUpload />
     </>
   );
 }
