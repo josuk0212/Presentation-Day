057b12c6f92dd728a55d9ac77579ea674315a736
JSLee <josuk0212@gmail.com>
2025-02-12 11:06:56 +0900

    feat: 발표자 페이지 생성 및 라우터 연결

---
+++ src/components/PresentationPage/SpeakerPage.jsx
@@ -0,0 +1,7 @@
+import DocumentViewer from "../ViewDocument/DocumentViewer";
+
+function SpeakerPage() {
+  return <DocumentViewer />;
+}
+
+export default SpeakerPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 import App from "./components/App.jsx";
+import SpeakerPage from "./components/PresentationPage/SpeakerPage.jsx";
 import "./style.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route
+          path="/"
+          element={<App />}
+        />
+        <Route
+          path="/Speaker"
+          element={<SpeakerPage />}
+        />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
