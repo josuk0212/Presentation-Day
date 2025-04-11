1b743a84c1150b5dbe27b1c540e3cf9d93577f8a
JSLee <josuk0212@gmail.com>
2025-04-11 15:36:04 +0900

    feat: 모바일 접속 불가 안내 문구 추가

--- src/components/MobilePage/MobilePage.jsx
+++ src/components/MobilePage/MobilePage.jsx
@@ -1,3 +1,17 @@
-function MobilePage() {}
+function MobilePage() {
+  return (
+    <div className="bg-background flex items-center justify-center min-h-screen px-4">
+      <div className="text-center">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-red-600 mb-4">
+          접근안내 🚫
+        </h1>
+        <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-gray-700">
+          이 페이지는 모바일에서 접근할 수 없습니다. <br />
+          PC로 이용해주시기 바랍니다.
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default MobilePage;
