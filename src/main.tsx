451fa0aabfd3108032545f03431dec010118c594
JSLee <josuk0212@gmail.com>
2025-05-27 18:27:33 +0900

    feat: main 파일 타입스크립트 변환 및 tailwind.config 타입스크립트 확장자명 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
-import App from "./components/App.jsx";
+import App from "./components/App";
 import "./style.css";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 import daisyui from "daisyui";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
